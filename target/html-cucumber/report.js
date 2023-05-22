511879409289afd1d61ffee4a560b6cdc0468369
Your Name <you@example.com>
2023-05-22 22:45:29 +0530

    saving phase 2 proj

---
+++ target/html-cucumber/report.js
@@ -0,0 +1,377 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/java/features/Addtocart.feature");
+formatter.feature({
+  "name": "This feature will be used to test the Add to Cart functionality of Swag Labs application",
+  "description": "",
+  "keyword": "Feature",
+  "tags": [
+    {
+      "name": "@AddToCart"
+    }
+  ]
+});
+formatter.background({
+  "name": "",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I have launched the application",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "stepDefs.Loginstepdef1.i_have_launched_the_application()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I enter username as \"standard_user\"",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "stepDefs.Loginstepdef1.i_enter_username_as(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I enter password as \"secret_sauce\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "stepDefs.Loginstepdef1.i_enter_password_as(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I click on Login button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "stepDefs.Loginstepdef1.i_click_on_Login_button()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I should land on home page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "stepDefs.Loginstepdef1.i_should_land_on_home_page()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "Add the product to Cart",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@AddToCart"
+    }
+  ]
+});
+formatter.step({
+  "name": "I click on the Product name as \"Sauce Labs Backpack\"",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "stepDefs.Addtocartstepdef.i_click_on_the_Product_name_as(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I click on the Add to cart Button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "stepDefs.Addtocartstepdef.i_click_on_the_Add_to_cart_Button()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I click on Shopping cart link",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "stepDefs.Addtocartstepdef.i_click_on_shopping_cart_link()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I verify the item on cart page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "stepDefs.Addtocartstepdef.i_verify_the_item_on_cart_page()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.uri("file:src/test/java/features/login1.feature");
+formatter.feature({
+  "name": "This feature will be used to test the login functionality of Swag Labs application",
+  "description": "",
+  "keyword": "Feature",
+  "tags": [
+    {
+      "name": "@Login"
+    }
+  ]
+});
+formatter.scenario({
+  "name": "Login success scenario using parameters",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@Login"
+    },
+    {
+      "name": "@sanity"
+    }
+  ]
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I have launched the application",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "stepDefs.Loginstepdef1.i_have_launched_the_application()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I enter username as \"standard_user\"",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "stepDefs.Loginstepdef1.i_enter_username_as(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I enter password as \"secret_sauce\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "stepDefs.Loginstepdef1.i_enter_password_as(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I click on Login button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "stepDefs.Loginstepdef1.i_click_on_Login_button()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I should land on home page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "stepDefs.Loginstepdef1.i_should_land_on_home_page()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.scenarioOutline({
+  "name": "Login Failure scenario using Scenario outline",
+  "description": "",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "name": "@regression"
+    }
+  ]
+});
+formatter.step({
+  "name": "I enter username as \"\u003cUserName\u003e\"",
+  "keyword": "When "
+});
+formatter.step({
+  "name": "I enter password as \"\u003cPassword\u003e\"",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "I click on Login button",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "I should get the error message as \"\u003cError\u003e\"",
+  "keyword": "Then "
+});
+formatter.examples({
+  "name": "",
+  "description": "",
+  "keyword": "Examples",
+  "rows": [
+    {
+      "cells": [
+        "UserName",
+        "Password",
+        "Error"
+      ]
+    },
+    {
+      "cells": [
+        "standard_user",
+        "xyz@12345",
+        "Epic sadface: Username and password do not match any user in this service"
+      ]
+    },
+    {
+      "cells": [
+        "locked_out_user",
+        "secret_sauce",
+        "Epic sadface: Sorry, this user has been locked out."
+      ]
+    }
+  ]
+});
+formatter.scenario({
+  "name": "Login Failure scenario using Scenario outline",
+  "description": "",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "name": "@Login"
+    },
+    {
+      "name": "@regression"
+    }
+  ]
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I enter username as \"standard_user\"",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "stepDefs.Loginstepdef1.i_enter_username_as(java.lang.String)"
+});
+formatter.result({
+  "error_message": "org.openqa.selenium.NoSuchElementException: no such element: Unable to locate element: {\"method\":\"xpath\",\"selector\":\"//input[@id\u003d\u0027user-name\u0027]\"}\n  (Session info: chrome\u003d113.0.5672.127)\nFor documentation on this error, please visit: https://www.seleniumhq.org/exceptions/no_such_element.html\nBuild info: version: \u00273.141.59\u0027, revision: \u0027e82be7d358\u0027, time: \u00272018-11-14T08:17:03\u0027\nSystem info: host: \u0027DESKTOP-JRLDGJM\u0027, ip: \u0027192.168.29.82\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u002717.0.6\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 113.0.5672.127, chrome: {chromedriverVersion: 113.0.5672.63 (0e1a4471d5ae..., userDataDir: C:\\Users\\lenovo\\AppData\\Loc...}, goog:chromeOptions: {debuggerAddress: localhost:59031}, javascriptEnabled: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: WINDOWS, platformName: WINDOWS, proxy: Proxy(), setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify, webauthn:extension:credBlob: true, webauthn:extension:largeBlob: true, webauthn:extension:minPinLength: true, webauthn:extension:prf: true, webauthn:virtualAuthenticators: true}\nSession ID: 9a26f61db6e983470eb66163ed1740bd\n*** Element info: {Using\u003dxpath, value\u003d//input[@id\u003d\u0027user-name\u0027]}\r\n\tat java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:77)\r\n\tat java.base/jdk.internal.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\r\n\tat java.base/java.lang.reflect.Constructor.newInstanceWithCaller(Constructor.java:499)\r\n\tat java.base/java.lang.reflect.Constructor.newInstance(Constructor.java:480)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:187)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:122)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:49)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:158)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:552)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:323)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElementByXPath(RemoteWebDriver.java:428)\r\n\tat org.openqa.selenium.By$ByXPath.findElement(By.java:353)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:315)\r\n\tat stepDefs.Loginstepdef1.i_enter_username_as(Loginstepdef1.java:23)\r\n\tat ✽.I enter username as \"standard_user\"(file:///C:/Users/lenovo/eclipse-workspace/Phase2Project/src/test/java/features/login1.feature:15)\r\n",
+  "status": "failed"
+});
+formatter.step({
+  "name": "I enter password as \"xyz@12345\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "stepDefs.Loginstepdef1.i_enter_password_as(java.lang.String)"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "I click on Login button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "stepDefs.Loginstepdef1.i_click_on_Login_button()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "I should get the error message as \"Epic sadface: Username and password do not match any user in this service\"",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "stepDefs.Loginstepdef1.i_should_get_the_error_message_as(java.lang.String)"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "Login Failure scenario using Scenario outline",
+  "description": "",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "name": "@Login"
+    },
+    {
+      "name": "@regression"
+    }
+  ]
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I enter username as \"locked_out_user\"",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "stepDefs.Loginstepdef1.i_enter_username_as(java.lang.String)"
+});
+formatter.result({
+  "error_message": "org.openqa.selenium.NoSuchElementException: no such element: Unable to locate element: {\"method\":\"xpath\",\"selector\":\"//input[@id\u003d\u0027user-name\u0027]\"}\n  (Session info: chrome\u003d113.0.5672.127)\nFor documentation on this error, please visit: https://www.seleniumhq.org/exceptions/no_such_element.html\nBuild info: version: \u00273.141.59\u0027, revision: \u0027e82be7d358\u0027, time: \u00272018-11-14T08:17:03\u0027\nSystem info: host: \u0027DESKTOP-JRLDGJM\u0027, ip: \u0027192.168.29.82\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u002717.0.6\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 113.0.5672.127, chrome: {chromedriverVersion: 113.0.5672.63 (0e1a4471d5ae..., userDataDir: C:\\Users\\lenovo\\AppData\\Loc...}, goog:chromeOptions: {debuggerAddress: localhost:59047}, javascriptEnabled: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: WINDOWS, platformName: WINDOWS, proxy: Proxy(), setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify, webauthn:extension:credBlob: true, webauthn:extension:largeBlob: true, webauthn:extension:minPinLength: true, webauthn:extension:prf: true, webauthn:virtualAuthenticators: true}\nSession ID: 7e8e3e06d9df4426da91c4e567f6bced\n*** Element info: {Using\u003dxpath, value\u003d//input[@id\u003d\u0027user-name\u0027]}\r\n\tat java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:77)\r\n\tat java.base/jdk.internal.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\r\n\tat java.base/java.lang.reflect.Constructor.newInstanceWithCaller(Constructor.java:499)\r\n\tat java.base/java.lang.reflect.Constructor.newInstance(Constructor.java:480)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:187)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:122)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:49)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:158)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:552)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:323)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElementByXPath(RemoteWebDriver.java:428)\r\n\tat org.openqa.selenium.By$ByXPath.findElement(By.java:353)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:315)\r\n\tat stepDefs.Loginstepdef1.i_enter_username_as(Loginstepdef1.java:23)\r\n\tat ✽.I enter username as \"locked_out_user\"(file:///C:/Users/lenovo/eclipse-workspace/Phase2Project/src/test/java/features/login1.feature:15)\r\n",
+  "status": "failed"
+});
+formatter.step({
+  "name": "I enter password as \"secret_sauce\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "stepDefs.Loginstepdef1.i_enter_password_as(java.lang.String)"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "I click on Login button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "stepDefs.Loginstepdef1.i_click_on_Login_button()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "I should get the error message as \"Epic sadface: Sorry, this user has been locked out.\"",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "stepDefs.Loginstepdef1.i_should_get_the_error_message_as(java.lang.String)"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.after({
+  "status": "passed"
+});
+});
